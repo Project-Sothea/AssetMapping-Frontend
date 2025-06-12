7817191685771d6a37e83dd24e2eecd59a62bb8a
chennieee <chenxin1812@gmail.com>
2025-06-12 08:43:31 +0800

    installing mapbox + token

---
+++ services/mapbox.ts
@@ -0,0 +1,3 @@
+import MapboxGL from '@rnmapbox/maps';
+MapboxGL.setAccessToken('pk.eyJ1IjoicHJvamVjdHNvdGhlYSIsImEiOiJjbWJzbjVxZngwbmZuMm5vZ3h0eXBnYnlrIn0.Ro_wtGTUXWsJ2cfjNdQ1iw');
+export default MapboxGL;
