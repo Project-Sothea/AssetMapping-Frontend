56b6b95dea8de7023a016743788c088dd2ff3f5c
chennieee <chenxin1812@gmail.com>
2025-06-14 15:23:21 +0800

    tryna open the map

--- app/index.tsx
+++ app/index.tsx
@@ -3,14 +3,14 @@ import { Text } from 'react-native';
 import { Button } from '~/components/Button';
 import { Container } from '~/components/Container';
 import { ScreenContent } from '~/components/ScreenContent';
+import Map from '~/components/Map'
 
 export default function Home() {
   return (
     <>
       <Stack.Screen options={{ title: 'Home' }} />
-      <Container>
         <Text>lucius was here lol</Text>
-      </Container>
+        <Map />
     </>
   );
 }
---
+++ components/Map.tsx
@@ -0,0 +1,7 @@
+import Mapbox, { MapView } from '@rnmapbox/maps'
+
+const accessToken = 'pk.eyJ1IjoicHJvamVjdHNvdGhlYSIsImEiOiJjbWJzbjVxZngwbmZuMm5vZ3h0eXBnYnlrIn0.Ro_wtGTUXWsJ2cfjNdQ1iw';
+Mapbox.setAccessToken(accessToken);
+export default function Map() {
+    return <MapView style = {{ flex: 1}}/>
+}
