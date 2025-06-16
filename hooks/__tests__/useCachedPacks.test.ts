4a489278e9caa13e1c05200e880405268448951a
lucius <luciusyeo18@gmail.com>
2025-06-16 22:25:57 +0800

    feat: create useCachedPacks and happy path testcase

--- hooks/__tests__/useCachedPacks.test.ts
+++ hooks/__tests__/useCachedPacks.test.ts
@@ -1,8 +1,7 @@
 import MapboxGL, { OfflineCreatePackOptions, offlineManager } from '@rnmapbox/maps';
 import { OfflineCreatePackOptionsArgs } from '@rnmapbox/maps/lib/typescript/src/modules/offline/OfflineCreatePackOptions';
-import { renderHook, waitFor } from '@testing-library/react-native';
+import { act, render, renderHook, waitFor } from '@testing-library/react-native';
 import { useCachedPacks } from '../useCachedPacks';
-import { DataState } from '../types';
 
 jest.mock('@rnmapbox/maps', () => ({
   offlineManager: {
@@ -11,56 +10,93 @@ jest.mock('@rnmapbox/maps', () => ({
   },
 }));
 
+const mockPack1 = {
+  name: 'myPack1',
+  bounds: [
+    [0, 0],
+    [1, 1],
+  ],
+  metadata: { someKey: 'someValue' },
+  status: jest.fn().mockResolvedValue({
+    name: 'myPack1',
+    state: 1,
+    percentage: 100,
+    completedResourceCount: 10,
+    completedResourceSize: 1024,
+    completedTileSize: 2048,
+    completedTileCount: 50,
+    requiredResourceCount: 50,
+  }),
+  resume: jest.fn().mockResolvedValue(undefined),
+  pause: jest.fn().mockResolvedValue(undefined),
+};
+
+const mockPack2 = {
+  name: 'myPack2',
+  bounds: [
+    [0, 0],
+    [1, 1],
+  ],
+  metadata: { someKey: 'someValue' },
+  status: jest.fn().mockResolvedValue({
+    name: 'myPack2',
+    state: 1,
+    percentage: 100,
+    completedResourceCount: 10,
+    completedResourceSize: 1024,
+    completedTileSize: 2048,
+    completedTileCount: 50,
+    requiredResourceCount: 50,
+  }),
+  resume: jest.fn().mockResolvedValue(undefined),
+  pause: jest.fn().mockResolvedValue(undefined),
+};
+
 describe('useCachedPacks', () => {
   describe('given cached packs exist', () => {
-    const mockPack = {
-      name: 'myPack',
-      bounds: [
-        [0, 0],
-        [1, 1],
-      ],
-      metadata: { someKey: 'someValue' },
-      status: jest.fn().mockResolvedValue({
-        name: 'myPack',
-        state: 1,
-        percentage: 100,
-        completedResourceCount: 10,
-        completedResourceSize: 1024,
-        completedTileSize: 2048,
-        completedTileCount: 50,
-        requiredResourceCount: 50,
-      }),
-      resume: jest.fn().mockResolvedValue(undefined),
-      pause: jest.fn().mockResolvedValue(undefined),
-    };
-
-    // const optionsArgs: OfflineCreatePackOptionsArgs = {
-    //   name: 'Sre O Primary School',
-    //   styleURL: 'mapbox://styles/mapbox/satellite-v9',
-    //   bounds: [
-    //     [103.390906, 12.829011], //neLg, neLat
-    //     [103.389445, 12.827979], //swLg, swLat
-    //   ],
-    //   minZoom: 16,
-    //   maxZoom: 18,
-    // };
-    // const mockPacks: OfflinePack[] = [new OfflinePack(new OfflineCreatePackOptions(optionsArgs))];
-
     test('when hook is mounted', async () => {
-      (offlineManager.getPacks as jest.Mock).mockResolvedValue([mockPack]);
+      const mockPacks = [mockPack1, mockPack2];
+
+      (offlineManager.getPacks as jest.Mock).mockResolvedValue(mockPacks);
 
       const { result } = renderHook(() => useCachedPacks());
 
       await waitFor(() => {
-        // expect(result.current.data).toEqual(mockOfflinePack);
-        expect(result.current.data).toEqual(mockPack);
-        expect(result.current.loading).toBeNull();
-        expect(result.current.error).toBeNull();
+        const dataState = result.current.dataState;
+        expect(dataState.data).toEqual(mockPacks);
+        expect(dataState.loading).toBe(false);
+        expect(dataState.error).toBeNull();
       });
     });
-    // test('when refetch func is called', () => {
-    //   expect(error).toBe(errorMsg);
-    // });
+
+    test('when refetch func is called', async () => {
+      const mockRes1 = [mockPack1];
+      const mockRes2 = [mockPack1, mockPack2];
+
+      (offlineManager.getPacks as jest.Mock)
+        .mockResolvedValueOnce(mockRes1) //initial call
+        .mockResolvedValueOnce(mockRes2); //refetch
+
+      const { result } = renderHook(() => useCachedPacks());
+
+      await waitFor(() => {
+        const dataState = result.current.dataState;
+        expect(dataState.data).toEqual(mockRes1);
+        expect(dataState.loading).toBe(false);
+        expect(dataState.error).toBeNull();
+      });
+
+      await act(async () => {
+        await result.current.refetch();
+      });
+
+      await waitFor(() => {
+        const dataState = result.current.dataState;
+        expect(dataState.data).toEqual(mockRes2);
+        expect(dataState.loading).toBe(false);
+        expect(dataState.error).toBeNull();
+      });
+    });
   });
   //   describe('given no cached packs exist', () => {
   //     test('when hook is mounted', () => {
--- hooks/useCachedPacks.ts
+++ hooks/useCachedPacks.ts
@@ -1,13 +1,62 @@
 import { useState, useCallback, useEffect } from 'react';
-import MapboxGL from '@rnmapbox/maps';
+import { offlineManager } from '@rnmapbox/maps';
 import { DataState } from './types';
 import OfflinePack from '@rnmapbox/maps/lib/typescript/src/modules/offline/OfflinePack';
 
 //to retrieve offline tile pack stored in the db
 //to be called as a function to retrieve tile pack
 
-export const useCachedPacks = (): DataState<OfflinePack[]> => {
+export const useCachedPacks = () => {
   //states
+  const [data, setData] = useState<any[] | null>(null);
+  const [error, setError] = useState<string | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
 
-  return { data: null, error: null, loading: false };
+  //Callback
+  const fetchPacks = useCallback(async () => {
+    setLoading(true);
+
+    try {
+      const res = await offlineManager.getPacks();
+      setData(res);
+      setError(null);
+    } catch (err) {
+      setError((err as Error).message);
+      setData(null);
+    } finally {
+      setLoading(false);
+    }
+  }, []);
+
+  //useEffect
+  useEffect(() => {
+    fetchPacks();
+  }, [fetchPacks]);
+
+  return { refetch: fetchPacks, dataState: { data: data, error: error, loading: loading } };
 };
+
+/*
+  const [data, setData] = useState<any[] | null>(null);
+  const [error, setError] = useState<Error | null>(null);
+  const [loading, setLoading] = useState(true);
+
+  const fetchPacks = useCallback(async () => {
+    setLoading(true);
+    try {
+      const packs = await offlineManager.getPacks();
+      setData(packs);
+      setError(null);
+    } catch (err) {
+      setError(err as Error);
+      setData(null);
+    } finally {
+      setLoading(false);
+    }
+  }, []);
+
+  useEffect(() => {
+    fetchPacks();
+  }, [fetchPacks]);
+
+*/
