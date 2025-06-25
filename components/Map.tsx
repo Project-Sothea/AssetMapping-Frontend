25df2f46de46db36e7e5a71143984d0fc8cce756
lucius <luciusyeo18@gmail.com>
2025-06-25 10:53:36 +0800

    fix: pin import

--- components/Map.tsx
+++ components/Map.tsx
@@ -1,6 +1,13 @@
-import Mapbox, { Camera, Images, LocationPuck, MapView, ShapeSource, SymbolLayer } from '@rnmapbox/maps';
+import Mapbox, {
+  Camera,
+  Images,
+  LocationPuck,
+  MapView,
+  ShapeSource,
+  SymbolLayer,
+} from '@rnmapbox/maps';
 import { featureCollection, point } from '@turf/helpers';
-import pin from '~/assets/pin.png';
+import pin from '../assets/pin.png';
 import homes from '~/data/homes.json';
 
 Mapbox.setAccessToken(process.env.EXPO_PUBLIC_MAPBOX_KEY || '');
@@ -13,18 +20,17 @@ export default function Map() {
     <MapView style={{ flex: 1 }} styleURL="mapbox://styles/mapbox/satellite-streets-v12">
       <Camera followUserLocation followZoomLevel={16}></Camera>
       <LocationPuck puckBearingEnabled puckBearing="heading" pulsing={{ isEnabled: true }} />
-      
-      <ShapeSource id = "houses" shape = {homesFeatures}>
+
+      <ShapeSource id="houses" shape={homesFeatures}>
         <SymbolLayer
-          id = "homes-icons"
-          style = {{
+          id="homes-icons"
+          style={{
             iconImage: 'pin',
-            iconSize: 0.5
+            iconSize: 0.5,
           }}
         />
-        <Images images = {{ pin }} />
+        <Images images={{ pin }} />
       </ShapeSource>
-
     </MapView>
   );
 }
