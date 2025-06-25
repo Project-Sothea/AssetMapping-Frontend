768efdca36d8d9cf6d6f1c7654386a5446fd43b0
chennieee <chenxin1812@gmail.com>
2025-06-25 10:25:30 +0800

    add pins on specific locations

--- components/Map.tsx
+++ components/Map.tsx
@@ -1,11 +1,30 @@
-import Mapbox, { Camera, LocationPuck, MapView } from '@rnmapbox/maps';
+import Mapbox, { Camera, Images, LocationPuck, MapView, ShapeSource, SymbolLayer } from '@rnmapbox/maps';
+import { featureCollection, point } from '@turf/helpers';
+import pin from '~/assets/pin.png';
+import homes from '~/data/homes.json';
 
 Mapbox.setAccessToken(process.env.EXPO_PUBLIC_MAPBOX_KEY || '');
+
 export default function Map() {
+  const points = homes.map((home) => point([home.long, home.lat]));
+  const homesFeatures = featureCollection(points);
+
   return (
     <MapView style={{ flex: 1 }} styleURL="mapbox://styles/mapbox/satellite-streets-v12">
       <Camera followUserLocation followZoomLevel={16}></Camera>
       <LocationPuck puckBearingEnabled puckBearing="heading" pulsing={{ isEnabled: true }} />
+      
+      <ShapeSource id = "houses" shape = {homesFeatures}>
+        <SymbolLayer
+          id = "homes-icons"
+          style = {{
+            iconImage: 'pin',
+            iconSize: 0.5
+          }}
+        />
+        <Images images = {{ pin }} />
+      </ShapeSource>
+
     </MapView>
   );
 }
