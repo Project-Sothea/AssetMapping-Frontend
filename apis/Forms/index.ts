3668cc688c86df7008472fc6cae6894fb734c778
lucius <luciusyeo18@gmail.com>
2025-06-29 01:45:19 +0800

    feat: add create, update, delete form

--- apis/Forms/index.ts
+++ apis/Forms/index.ts
@@ -1,3 +1,4 @@
+import { Form } from '~/utils/database.types';
 import { supabase } from '~/utils/supabase';
 
 export const getForms = async () => {
@@ -25,5 +26,36 @@ export const getForm = async (id: string) => {
   } catch (err) {
     console.error('Non-Supabase Error:', err);
     return null;
+  }
+};
+
+export const createForm = async (form: Form) => {
+  try {
+    const { error } = await supabase.from('forms').insert(form);
+    if (error) {
+      console.error('supabase error:', error.message);
+    }
+  } catch (err) {
+    console.error('Non-Supabase Error:', err);
+  }
+};
+
+export const updateForm = async (id: string, values: Partial<Form>) => {
+  try {
+    const { error } = await supabase.from('forms').update(values).eq('id', id);
+    if (error) {
+      console.error('supabase error:', error.message);
+    }
+  } catch (err) {
+    console.error('Non-Supabase Error:', err);
+  }
+};
+
+export const deleteForm = async (id: string) => {
+  try {
+    const response = await supabase.from('forms').delete().eq('id', id);
+    return response;
+  } catch (err) {
+    console.error('Non-Supabase Error:', err);
   }
 };
--- hooks/Forms/index.ts
+++ hooks/Forms/index.ts
@@ -1,5 +1,5 @@
-import { useQuery } from '@tanstack/react-query';
-import { getForm, getForms } from '~/apis/Forms';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
+import { createForm, deleteForm, getForm, getForms, updateForm } from '~/apis/Forms';
 import { Form } from '~/utils/database.types';
 
 export const useBulkFetchForms = () => {
@@ -13,5 +13,38 @@ export const useFetchForm = (id: string) => {
   return useQuery<Form | null>({
     queryKey: ['form', id],
     queryFn: () => getForm(id),
+  });
+};
+
+export const useCreateForm = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: (form: Form) => createForm(form),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['forms'] });
+    },
+  });
+};
+
+export const useUpdateForm = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: ({ id, values }: { id: string; values: Partial<Form> }) => updateForm(id, values),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['forms'] });
+    },
+  });
+};
+
+export const useDeleteForm = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: (id: string) => deleteForm(id),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['forms'] });
+    },
   });
 };
