cd4f90f1d7508ccb3d2f05675a5e0eff9e70df32
lucius <luciusyeo18@gmail.com>
2025-11-12 09:33:13 +0800

    Remove old files

--- services/drizzleDb.ts
+++ services/drizzleDb.ts
@@ -4,9 +4,6 @@ import { drizzle as drizzleSqlite } from 'drizzle-orm/expo-sqlite';
 import { forms, pins } from '~/db/schema/sqlite';
 import { useMigrations } from 'drizzle-orm/expo-sqlite/migrator';
 import migrations from '../drizzle/sqlite/migrations';
-import { PgTable } from 'drizzle-orm/pg-core';
-import { SQLiteTable } from 'drizzle-orm/sqlite-core';
-import { getTableColumns, SQL, sql } from 'drizzle-orm';
 
 //Local DB
 const DATABASE_NAME = 'local.db';
@@ -17,48 +14,3 @@ export const db = drizzleSqlite(expoDb, { schema: { pins, forms } }); //add in a
 export function useRunMigrations() {
   return useMigrations(db, migrations);
 }
-
-//custom functions
-export function buildUpsertSet<Table extends Record<string, any>>(
-  table: Table,
-  excludedCols: (keyof Table)[] = []
-): Record<keyof Table, SQL> {
-  const columns = Object.keys(table) as (keyof Table)[];
-  const set = columns
-    .filter((col) => !excludedCols.includes(col))
-    .reduce(
-      (acc, col) => {
-        const columnName = (table[col] as any).name;
-        acc[col] = sql.raw(`excluded.${columnName}`);
-        return acc;
-      },
-      {} as Record<keyof Table, SQL>
-    );
-
-  return set;
-
-  // for (const key in table) {
-  //     //if excludedCols, then dont add to set
-  //     if (!excludedCols.includes(key)) {
-  //       set[key as keyof Table] = sql.raw(`excluded.${colName}`);
-  //     }
-  // }
-}
-
-export const buildConflictUpdateColumns = <
-  T extends PgTable | SQLiteTable,
-  Q extends keyof T['_']['columns'],
->(
-  table: T,
-  columns: Q[]
-) => {
-  const cls = getTableColumns(table);
-  return columns.reduce(
-    (acc, column) => {
-      const colName = cls[column].name;
-      acc[column] = sql.raw(`excluded.${colName}`);
-      return acc;
-    },
-    {} as Record<Q, SQL>
-  );
-};
